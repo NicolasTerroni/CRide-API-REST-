601256f93c7ecc5b17f3b0ee438f056c235bae8b
nicolasterroni <nsterroni@gmail.com>
2021-03-09 19:48:08 -0300

    Circle's app
    Circle's app, model and admin configuration.

---
+++ cride/circles/apps.py
@@ -0,0 +1,9 @@
+"""Circles app."""
+
+# Django
+from django.apps import AppConfig
+
+class CirclesAppConfig(AppConfig):
+    """Circle's app configuration."""
+    name = "cride.circles"
+    verbose_name = "Circles"
