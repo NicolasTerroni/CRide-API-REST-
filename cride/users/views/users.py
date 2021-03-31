8c5da3b4507a5b62dfe93d76e2d60d91d0da1f20
nicolasterroni <nsterroni@gmail.com>
2021-03-31 17:03:50 -0300

    Users login with authtoken

---
+++ cride/users/views/users.py
@@ -0,0 +1,26 @@
+"""Users view"""
+
+# Django REST Framework
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+ 
+ # Serializers
+from cride.users.serializers import UserLoginSerializer, UserModelSerializer
+
+class UserLoginAPIView(APIView):
+    """User login API View"""
+    
+    def post(self, request, *args, **kwargs):
+        """Handle HTTP POST request."""
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user, token = serializer.save()
+
+        data = {
+            'user': UserModelSerializer(user).data,
+            'access_token': token,
+        }
+
+        return Response(data, status=status.HTTP_201_CREATED)
