eeb1ef7083ef10090d7dce5e4cd9e049d328f791
nicolasterroni <nsterroni@gmail.com>
2021-04-03 18:39:07 -0300

    Account verification with JWT
    endpoint, token generator, view and serializer

--- cride/users/urls.py
+++ cride/users/urls.py
@@ -1,9 +1,10 @@
 """Users urls"""
 
 from django.urls import path
-from cride.users.views import UserLoginAPIView,UserSignUpAPIView
+from cride.users.views import UserLoginAPIView,UserSignUpAPIView,UserAccountVerificationAPIView
 
 urlpatterns = [
     path('users/login/', UserLoginAPIView.as_view(),name='login'),
     path('users/signup/', UserSignUpAPIView.as_view(),name='signup'),
+    path('users/verify/', UserAccountVerificationAPIView.as_view(),name='verify'),
 ]
--- cride/users/views/users.py
+++ cride/users/views/users.py
@@ -6,7 +6,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
  
  # Serializers
-from cride.users.serializers import UserLoginSerializer, UserModelSerializer, UserSignUpSerializer
+from cride.users.serializers import (
+    UserLoginSerializer, 
+    UserModelSerializer, 
+    UserSignUpSerializer,
+    AccountVerificationSerializer,
+)
 
 class UserLoginAPIView(APIView):
     """User login API View"""
@@ -41,3 +46,20 @@ class UserSignUpAPIView(APIView):
         }
 
         return Response(data, status=status.HTTP_201_CREATED)
+
+
+class UserAccountVerificationAPIView(APIView):
+    """Account verification API View"""
+    
+    def post(self, request, *args, **kwargs):
+        """Handle HTTP POST request."""
+        serializer = AccountVerificationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        data = {
+            'message': 'Congratulation, now go share some rides!',
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
