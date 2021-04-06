452fa8a21d708ebc3cb910de6fbcd53e1fbdc632
nicolasterroni <nsterroni@gmail.com>
2021-04-06 15:06:33 -0300

    Circle router, circle viewset, circle model serializer.

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -1,9 +1,15 @@
 """Circles urls"""
 
-from django.urls import path
-from cride.circles.views import list_circles, create_circle
+# Django
+from django.urls import path, include
+# Django REST Framework
+from rest_framework.routers import DefaultRouter
+# Views
+from .views import circles as circle_views
+
+router = DefaultRouter()
+router.register(r'circles',circle_views.CircleViewSet, basename='circle')
 
 urlpatterns = [
-    path('circles/', list_circles),
-    path('circles/create/', create_circle),
+    path('', include(router.urls))
 ]
