2a1aaf679f105a102e0e0d287d80ff8dd576dc6d
nicolasterroni <nsterroni@gmail.com>
2021-04-11 19:15:50 -0300

    List member invitations shows remainin invitations

--- cride/circles/views/memberships.py
+++ cride/circles/views/memberships.py
@@ -7,7 +7,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
 # Models
-from cride.circles.models import Circle, Membership
+from cride.circles.models import Circle, Membership, Invitation
 
 # Serializers
 from cride.circles.serializers import MembershipModelSerializer
@@ -65,12 +65,33 @@ class MembershipViewSet(mixins.ListModelMixin,
         have used its invitations and another list containing
         the invitations taht haven't been used yet.
         """
+        member = self.get_object()
+
         invited_members = Membership.objects.filter(
             circle=self.circle,
             invited_by=request.user,
             is_active=True,
         )
+
+        unused_invitations = Invitation.objects.filter(
+            circle=self.circle,
+            issued_by=request.user,
+            used=False,
+        ).values_list('code')
+
+        diff = member.remaining_invitations - len(unused_invitations)
+
+        invitations = [i[0] for i in unused_invitations]
+        for i in range(0, diff):
+            invitations.append(
+                Invitation.objects.create(
+                    issued_by=request.user,
+                    circle=self.circle,
+                ).code
+            )
+
         data = {
-            'used_invitations': MembershipModelSerializer(invited_members,many=True).data
+            'used_invitations': MembershipModelSerializer(invited_members,many=True).data,
+            'invitations': invitations
         }
         return Response(data)
