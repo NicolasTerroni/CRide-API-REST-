7d36e42ef4eaba5585bbbd0462b979ad41139a0d
nicolasterroni <nsterroni@gmail.com>
2021-04-12 12:16:34 -0300

    Join to a circle with invitation code. AddMemberSerializer, IsSelfMember permission, membership viewset functions.

--- cride/circles/permissions/memberships.py
+++ cride/circles/permissions/memberships.py
@@ -1,6 +1,6 @@
 """Membership custom permissions."""
 # Models
-from cride.circles.models import Membership
+from cride.circles.models import Membership, Invitation
 # Django REST Framework
 from rest_framework.permissions import BasePermission
 
@@ -23,3 +23,16 @@ class IsActiveCircleMember(BasePermission):
         except Membership.DoesNotExist:
             return False
         return True
+
+
+class IsSelfMember(BasePermission):
+    """Allow access only to member owners."""
+
+    def has_permission(self,request,view):
+        """Let objects permission grant access."""
+        obj = view.get_object()
+        return self.has_object_permission(request,view,obj)
+
+    def has_object_permission(self,request,view,obj):
+        """Allow acces only if member is owned by the requesting user."""
+        return request.user == obj.user
