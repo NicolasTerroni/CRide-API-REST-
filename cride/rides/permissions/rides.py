a7ecc941c13a5c99ca18523e38e6a16b9ce6c09d
nicolasterroni <nsterroni@gmail.com>
2021-04-22 16:24:42 -0300

    Now ride owners cant join their own rides.

--- cride/rides/permissions/rides.py
+++ cride/rides/permissions/rides.py
@@ -9,3 +9,12 @@ class IsRideOwner(BasePermission):
     def has_object_permission(self,request,view,obj):
         """Verify requesting user is the ride publisher."""
         return request.user == obj.offered_by
+
+class IsNotRideOwner(BasePermission):
+    """Verify requesting user is not the ride publisher."""
+
+    message = "The ride owner can't be a passenger."
+
+    def has_object_permission(self,request,view,obj):
+        """Verify requesting user is not the ride publisher."""
+        return request.user != obj.offered_by
--- cride/rides/views/rides.py
+++ cride/rides/views/rides.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 # Permissions
 from rest_framework.permissions import IsAuthenticated
 from cride.circles.permissions.memberships import IsActiveCircleMember
-from cride.rides.permissions.rides import IsRideOwner
+from cride.rides.permissions.rides import IsRideOwner, IsNotRideOwner
 
 # Filters
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -48,8 +48,13 @@ class RideViewSet(  AddCircleMixin,
     def get_permissions(self):
         """Assign permissions based on action."""
         permissions = [IsAuthenticated, IsActiveCircleMember]
+
         if self.action in ['update','partial_update']:
             permissions.append(IsRideOwner)
+
+        if self.action == 'join':
+            permissions.append(IsNotRideOwner)
+
         return [permission() for permission in permissions]
 
     def get_serializer_context(self):
